a4f8f125fbb9c0e221d86f41745188ef42f81c09
Haichao Yu <haichaoy@asu.edu>
2017-05-30 11:21:35 -0700

    parsing data through props

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 
 class Square extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            value: null,
+        };
+    }
+
+    /*
+     * Whenever this.setState() is called, an update to the component is scheduled,
+     * causing React to merge in the passed state update and rerender the component
+     * along with its descendants.
+     */
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
@@ -18,7 +31,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i} />;
     }
 
     render() {
